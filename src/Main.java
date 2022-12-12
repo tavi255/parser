c3bf94d89e6a079234f0b63456a40b84be00fed6
917_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
2022-12-12 03:14:13 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
         Parser p=new Parser(g);
 
+
         Map<List<String>, List<String>> closure=p.closure("S' -> .S");
 
         StringBuilder sb=new StringBuilder();
@@ -35,21 +36,51 @@ public class Main {
         for(List<String>k: gto.keySet())
         {
             String key=k.get(0);
-            sb.append(key).append(" -> ");
+            sb2.append(key).append(" -> ");
             for(String v: gto.get(k))
             {
-                sb.append(v).append("|");
+                sb2.append(v).append("|");
             }
 
-            sb.replace(sb.length()-1,sb.length(),"\n");
+            sb2.replace(sb2.length()-1,sb2.length(),"\n");
 
         }
 
-        System.out.println(sb);
+        System.out.println(sb2);
 
 
+        List<Map<List<String>, List<String>>>canonicaCol=p.canocicalCol();
+
+        int i=0;
+
+        for(Map<List<String>,List<String>>iteration:canonicaCol)
+        {
+            System.out.println("Iteration: "+i);
+
+            StringBuilder sb3=new StringBuilder();
+
+            for(List<String>k: iteration.keySet())
+            {
+                String key=k.get(0);
+                sb3.append(key).append(" -> ");
+                for(String v: iteration.get(k))
+                {
+                    sb3.append(v).append("|");
+                }
+
+                sb3.replace(sb3.length()-1,sb3.length(),"\n");
+
+            }
+
+            System.out.println(sb3);
+
+
+            i++;
+        }
 
     }
+
+
 
 
 
